5498ddbe585de02b6cf641960befdca1d99fd815
Moe <moepoi@protonmail.com>
2019-07-05 11:08:54 +0700

    Update example >///<

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from HTV import HanimeTV
 
-hentai = HanimeTV()
+hentai = HanimeTV(email="EMAIL", password="PASS")
 query = "Imouto Paradise"
 url = "https://hanime.tv/hentai-videos/imouto-paradise-1-ep-1"
 
@@ -16,6 +16,6 @@ print (info)
 storyboards = hentai.storyboards(url)
 print (storyboards)
 
-# GET DOWNLOAD (STILL NOT WORKING)
+# GET DOWNLOAD (USING AUTH)
 download = hentai.download(url)
 print (download)
